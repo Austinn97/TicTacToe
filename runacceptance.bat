9c24a95e1a8c43ab02e1595bc53e245be98a70ee
Austinn97 <2015ada@gmail.com>
2016-09-07 22:04:34 -0500

    Add files via upload

---
+++ runacceptance.bat
@@ -0,0 +1,5 @@
+cd acceptance
+javac -cp .;..C:\Users\2015a_000\Desktop\School\Fall 2016\Software Engineering\tic-tac-toe TicTacToe.java
+java -cp .;..\build\classes\main;C:\Users\Robot_Framework\robotframework-2.9.jar org.robotframework.RobotFramework TicTacToeTests.txt
+cd ..
+pause
